aaa5902251935dd5969863cebd31ccee48a6b963
zhanzghiyong0410 <2556703570@qq.com>
2020-10-29 10:32:24 +0800

    the first time

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'technicalDocumentation'
-copyright = '2020, john'
-author = 'john'
+project = 'EzblockReadtheDocs'
+copyright = '2020, sunfounder'
+author = 'sunfounder'
 
 
 # -- General configuration ---------------------------------------------------
@@ -44,7 +44,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
