079a6256abea22f456196f11bf395ba2362032d9
SunFounder <support@sunfounder.com>
2021-05-21 09:38:04 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -57,3 +59,10 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 source_suffix = ['.rst', '.md']
+
+html_js_files = [
+    'https://ezblock.cc/readDocFile/topHead.js',
+]
+html_css_files = [
+    'https://ezblock.cc/readDocFile/topHead.css',
+]
